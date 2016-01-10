8299774291d50d6783d12c8fa1b58475d304bd62
саша <shaitanaleksander@gmail.com>
2016-01-10 20:31:17 +0300

    go

--- game.js
+++ game.js
@@ -13,7 +13,10 @@
         };
 
         this.bulletNumber = 0;
-        this.bodies = [new Player(this, gameSize)];
+
+        this.bulletArray = [];
+
+        this.bodies = createInvaders(this).concat([new Player(this, gameSize)]);
         var self = this;
 
         var tick = function () {
@@ -29,29 +32,77 @@
 //###############################
     Game.prototype = {
 
+
         update: function (gameSize) {
 
-            console.log(this.bodies.length);
-            for (var i = 0; i < this.bodies.length; i++) {
-                if(this.bodies[i].position.y < 0){
-                    this.bodies.splice(i, 1);
+              function isKilled(bodie,bulletArray) {
+
+                for (var i = 0; i < bulletArray.length; i++) {
+                    if ((bulletArray[i].position.x == bodie.position.x) && (bulletArray[i].position.y == bodie.position.y)) {
+                        return true;
+                    }
                 }
+                console.log("gogogogo");
+                return false;
 
             }
+
+            console.log(this.bulletArray.length);
+            for (var i = 0; i < this.bulletArray.length; i++) {
+
+                this.bulletArray[i].update();
+
+                if (this.bulletArray[i].position.y < 0) {
+                    this.bulletArray.splice(i, 1);
+                }
+            }
             for (var i = 0; i < this.bodies.length; i++) {
-                this.bodies[i].update()
+                this.bodies[i].update();
+                if(isKilled(this.bodies[i],this.bulletArray)){
+                    this.bodies.splice(i, 1);
+                }
             }
         },
+
         draw: function (screen, gameSize) {
             clearCanvas(screen, gameSize);
             for (var i = 0; i < this.bodies.length; i++) {
                 drawRect(screen, this.bodies[i]);
             }
+            for (var i = 0; i < this.bulletArray.length; i++) {
+                drawRect(screen, this.bulletArray[i]);
+            }
         },
+
 
         addBody: function (body) {
             this.bodies.push(body);
         }
+    };
+
+
+    var Invader = function (game, position) {
+
+        this.game = game;
+        this.size = {width: 16, height: 16};
+        this.position = position;
+        this.patrolX = 0;
+        this.speadX = 1;
+    };
+
+
+    Invader.prototype = {
+
+        update: function () {
+            if (this.patrolX < 0 || this.patrolX > 200) {
+                this.speadX = -this.speadX;
+            }
+
+            this.position.x += this.speadX;
+            this.patrolX += this.speadX;
+
+        }
+
     };
 
 //#########################################
@@ -76,16 +127,16 @@
             }
             if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
 
-                if (this.game.bulletNumber < Math.random()*10) {
+                if (this.game.bulletNumber < Math.random() * 10) {
                     var bullet = new Bullet({
                         x: this.position.x + this.size.width / 2 - 3 / 2,
                         y: this.position.y
                     }, {x: 0, y: -6});
-                    this.game.addBody(bullet);
+                    this.game.bulletArray.push(bullet);
                     this.game.bulletNumber++;
                 }
                 else {
-                    this.game.bulletNumber = Math.random()*10;
+                    this.game.bulletNumber = Math.random() * 10;
                 }
 
             }
@@ -119,7 +170,7 @@
         };
         window.onkeyup = function (e) {
             keyState[e.keyCode] = false;
-        } ;
+        };
 
         this.isDown = function (keyCode) {
             return keyState[keyCode] === true;
@@ -128,6 +179,17 @@
         this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
     };
 
+
+    var createInvaders = function (game) {
+        var invaders = [];
+        for (var i = 0; i < 48; i++) {
+
+            var x = 30 + (i % 16) * 30;
+            var y = 30 + (i % 3) * 30;
+            invaders.push(new Invader(game, {x: x, y: y}));
+        }
+        return invaders;
+    };
 //################################################
     var drawRect = function (screen, body) {
         screen.fillRect(body.position.x, body.position.y, body.size.width, body.size.height);
